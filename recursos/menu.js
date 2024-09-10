79aae04a0e8520290fb5ac40890a626c5c1645c1
lagarto2k7 <eduardoescobar2k7@gmail.com>
2024-09-10 10:41:10 -0500

    ajustes responsive

--- recursos/menu.js
+++ recursos/menu.js
@@ -7,7 +7,7 @@ $(function(){
 	$(window).on('load resize',function(){
 		var w = $(window).width();
 		var h = $(window).height();
-		var x = 1040;
+		var x = 1000;
 		if (w >= x) {
 			$('nav ul').css({ display: 'flex',height: 'auto' });
 		}else {
